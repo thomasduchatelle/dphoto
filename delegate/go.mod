a2097e08204634b7b582edef498b2cc463df735d
Thomas Duchatelle <duchatelle.thomas@gmail.com>
2020-10-04 23:18:38 +0100

    delegate - only log plug in and plug out events
    * introduce logging library
    * parse args to setup logging levels
    * extract DBus / UDisk in a separate source file

--- delegate/go.mod
+++ delegate/go.mod
@@ -2,4 +2,7 @@ module duchatelle.io/dphoto/delegate
 
 go 1.15
 
-require github.com/godbus/dbus/v5 v5.0.3
+require (
+	github.com/godbus/dbus/v5 v5.0.3
+	github.com/sirupsen/logrus v1.7.0
+)
---
+++ delegate/logging_init.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+var debug bool
+var trace bool
+
+func loggingArgs() {
+	flag.BoolVar(&debug, "debug", false, "enable debug output")
+	flag.BoolVar(&debug, "v", false, "enable debug output")
+	flag.BoolVar(&trace, "trace", false, "enable trace output")
+	flag.BoolVar(&trace, "vv", false, "enable trace output")
+}
+
+func loggingInit() {
+	log.SetOutput(os.Stdout)
+	formatter := new(log.TextFormatter)
+	formatter.FullTimestamp = true
+	log.SetFormatter(formatter)
+
+	if trace {
+		log.SetLevel(log.TraceLevel)
+	} else if debug {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
+
+	log.WithFields(log.Fields{
+		"debug": debug,
+		"trace": trace,
+	}).Traceln("Logging initiated")
+}
