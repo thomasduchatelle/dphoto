be77cb93e84ac3ec798860da9f8610bd6ab6cf68
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2023-01-07 15:22:02 +0000

    Release 1.3.0 with update in Github Actions to tag as 'v1.3.0'

--- pkg/meta/version.go
+++ pkg/meta/version.go
@@ -2,7 +2,7 @@
 package meta
 
 const (
-	version = "1.2.0"
+	version = "1.3.0"
 )
 
 // Version returns the version of the app, updated by ci/pre-release.sh
--- web/src/components/AppNav/index.tsx
+++ web/src/components/AppNav/index.tsx
@@ -1,7 +1,7 @@
 import {AppBar, Box, Container, Toolbar, useScrollTrigger} from "@mui/material";
 import {cloneElement, ReactElement, ReactNode} from "react";
 
-const appVersion = "1.2.0"
+const appVersion = "1.3.0"
 
 const ElevationScroll = ({children}: {
   children: ReactElement;
