b1304383d593f7abd0149627ccd47d059fc8ed34
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2024-04-28 20:43:02 +0100

    aws - run the DynamoDB table update each time the Cloud Formation stack is updated (using custom resourecs) +next

--- api/lambdas/sys-dynamodb-upgrade/lambda.go
+++ api/lambdas/sys-dynamodb-upgrade/lambda.go
@@ -2,28 +2,76 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/cfn"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/thomasduchatelle/dphoto/api/lambdas/common"
 	"github.com/thomasduchatelle/dphoto/pkg/awssupport/appdynamodb"
 	"github.com/thomasduchatelle/dphoto/pkg/awssupport/awsfactory"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"strings"
 )
 
-func Handler() error {
-	ctx := context.Background()
-	factory, err := awsfactory.NewAWSFactory(ctx, awsfactory.DefaultConfigFactory)
-	if err != nil {
-		return err
-	}
-
+func Handler(event *cfn.Event) error {
 	table := viper.GetString(common.DynamoDBTableName)
 	if table == "" {
-		return errors.Errorf("'%s' environment variable is required with the name of the table.", common.DynamoDBTableName)
+		return handleError(event, "DBStructureNoID", errors.Errorf("'%s' environment variable is required with the name of the table.", common.DynamoDBTableName))
 	}
-	return appdynamodb.CreateTableIfNecessary(ctx, table, factory.GetDynamoDBClient(), false)
+	physicalResourceID := fmt.Sprintf("DBStructure%s", strings.ReplaceAll(cases.Title(language.English).String(table), "-", ""))
+
+	if event.RequestType == cfn.RequestCreate || event.RequestType == cfn.RequestUpdate {
+		log.WithField("PhysicalResourceID", physicalResourceID).
+			WithField("event.PhysicalResourceID", event.PhysicalResourceID).
+			Infof("Handling %s request.", event.RequestType)
+
+		ctx := context.Background()
+		factory, err := awsfactory.NewAWSFactory(ctx, awsfactory.DefaultConfigFactory)
+		if err != nil {
+			return handleError(event, physicalResourceID, errors.Wrapf(err, "intialisation failed"))
+		}
+
+		err = appdynamodb.CreateTableIfNecessary(ctx, table, factory.GetDynamoDBClient(), false)
+		if err != nil {
+			return handleError(event, physicalResourceID, errors.Wrapf(err, "table structure update failed"))
+		}
+
+		return handleSuccess(event, physicalResourceID)
+
+	} else {
+		log.WithField("event.PhysicalResourceID", event.PhysicalResourceID).
+			Infof("%s RequestType ignored", event.RequestType)
+		return handleSuccess(event, physicalResourceID)
+	}
 }
 
 func main() {
 	lambda.Start(Handler)
+}
+
+func handleSuccess(event *cfn.Event, physicalResourceID string) error {
+	response := cfn.NewResponse(event)
+	response.PhysicalResourceID = physicalResourceID
+	response.Status = cfn.StatusSuccess
+	response.Reason = "Update completed."
+
+	return response.Send()
+}
+
+func handleError(event *cfn.Event, physicalResourceID string, err error) error {
+	response := cfn.NewResponse(event)
+	response.PhysicalResourceID = physicalResourceID
+	response.Status = cfn.StatusFailed
+	response.Reason = err.Error()
+
+	sendErr := response.Send()
+	if sendErr != nil {
+		log.WithError(err).Error("Failed to ack the Cloud Formation event")
+		return errors.Wrapf(err, "failed to send response dur to [%s], migration original failed because", sendErr.Error())
+	}
+
+	return err
 }
