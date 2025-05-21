9a47be06462cd210d44153fd3ceaf83786e6f262
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2025-05-21 21:29:05 +0100

    acl/ui - make controller stateless to fix issue where the SHARE request is not send to the backend. +next

--- web/src/pages/authenticated/albums/share-controller/useSharingModalController.ts
+++ web/src/pages/authenticated/albums/share-controller/useSharingModalController.ts
@@ -2,18 +2,49 @@ import {useMemo, useReducer} from "react";
 import {ShareController} from "./ShareController";
 import {ShareState, sharingModalReducer} from "./sharingModalReducer";
 import {useCatalogAPIAdapter} from "../../../../core/catalog-react";
+import {Album, SharingType} from "../../../../core/catalog";
 
-export function useSharingModalController(): ShareState & ShareController {
+export interface ShareCallbacks {
+
+    onRevoke(email: string): Promise<void>
+
+    onGrant(email: string, role: SharingType): Promise<void>
+
+    openSharingModal(album: Album): void
+
+    onClose(): void
+}
+
+export function useSharingModalController(): ShareState & ShareCallbacks {
     const [state, dispatch] = useReducer(sharingModalReducer, {
         open: false,
         sharedWith: [],
     })
     const catalogAPIAdapter = useCatalogAPIAdapter()
 
-    const ctrl = useMemo(() => new ShareController(dispatch, catalogAPIAdapter), [dispatch, catalogAPIAdapter])
+    const callbacks = useMemo(() => {
+        const ctrl = new ShareController(dispatch, catalogAPIAdapter);
+        return {
+            ...ctrl,
+            onGrant: (email: string, role: SharingType) => {
+                if (!state.sharedAlbumId) {
+                    return Promise.reject(`ERROR: no albumId selected to be granted, cannot grant access for ${email}`)
+                }
+
+                return ctrl.grantAccess(state.sharedAlbumId, email, role)
+            },
+            onRevoke: (email: string) => {
+                if (!state.sharedAlbumId) {
+                    return Promise.reject(`ERROR: no albumId selected to be revoked, cannot revoke access for ${email}`)
+                }
+
+                return ctrl.revokeAccess(state.sharedAlbumId, email)
+            },
+        }
+    }, [dispatch, catalogAPIAdapter, state.sharedAlbumId])
 
     return {
-        ...ctrl,
+        ...callbacks,
         ...state
     }
 }
