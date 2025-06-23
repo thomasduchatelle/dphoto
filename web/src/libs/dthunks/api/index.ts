8ef934ed7e2181699d27c45c917a5ad9395b3fc6
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2025-06-23 09:48:05 +0100

    dthunks `createSimpleThunkDeclaration`

--- web/src/libs/dthunks/api/index.ts
+++ web/src/libs/dthunks/api/index.ts
@@ -1,3 +1,9 @@
+/**
+ * A thunk is a function triggered by a user interaction which execute the business logic (API calls, actions, ...). It takes all its dependencies as arguments.
+ *
+ * Declaring the thunks with these interface is used to convert it into a stable handler ready to be called by the UI callbacks.
+ */
+
 export interface ThunkFactoryExtraArgs<PartialState> {
     partialState: PartialState
 }
@@ -12,4 +18,23 @@ export interface ThunkFactoryExtraArgs<PartialState> {
 export interface ThunkDeclaration<State, PartialState, F extends Function, FactoryArgs> {
     factory: (args: FactoryArgs & ThunkFactoryExtraArgs<PartialState>) => F;
     selector: (state: State) => PartialState;
+}
+
+type ActionCreator<TAction, TArgs extends any[]> = (...args: TArgs) => TAction;
+
+/**
+ * Shorthand to implement the declaration of a thunk dispatching immediately and always an action. The thunk tooling gives a stable handler with the dispatch injected.
+ * @param actionCreator function to create the action (using createAction from daction lib)
+ */
+export function createSimpleThunkDeclaration<TAction, TArgs extends any[]>(
+    actionCreator: ActionCreator<TAction, TArgs>
+): ThunkDeclaration<any, {}, (...args: TArgs) => void, { dispatch: (action: TAction) => void }> {
+    return {
+        selector: () => ({}),
+        factory: ({dispatch}) => {
+            return (...args: TArgs) => {
+                dispatch(actionCreator(...args));
+            };
+        },
+    };
 }
