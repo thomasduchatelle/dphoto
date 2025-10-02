c1e3c742697d692524238e7358822978826d2ef2
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2025-10-02 12:08:51 +0100

    start ladle with playwright

--- web-waku/playwright.config.ts
+++ web-waku/playwright.config.ts
@@ -1,6 +1,6 @@
 export default {
-    // webServer: {
-    //     command: "npm run ladle",
-    //     url: `http://localhost:61000`,
-    // },
+    webServer: {
+        command: "npm run ladle",
+        url: `http://localhost:61000`,
+    },
 };
