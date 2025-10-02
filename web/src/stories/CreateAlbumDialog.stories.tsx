d80925dc13228ce170dad624e40ad15d92b89bf6
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2025-10-02 19:08:49 +0100

    web - migrate storybook to ladle.

--- web/src/stories/CreateAlbumDialog.stories.tsx
+++ web/src/stories/CreateAlbumDialog.stories.tsx
@@ -1,125 +1,90 @@
 import React from 'react';
-import {ComponentMeta, ComponentStory} from '@storybook/react';
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import {LocalizationProvider} from '@mui/x-date-pickers';
 import dayjs from "dayjs";
 import fr from "dayjs/locale/fr";
 import {CreateAlbumDialog} from "../pages/authenticated/albums/CreateAlbumDialog";
+import {Story} from "@ladle/react";
+import {CreateDialogSelection} from "../core/catalog";
 
 dayjs.locale(fr)
-
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
-    title: 'Albums/CreateAlbumDialog',
-    component: CreateAlbumDialog,
-} as ComponentMeta<typeof CreateAlbumDialog>;
 
 const defaultStartDate = new Date("2024-12-21T00:00:00Z")
 const endDate = new Date("2024-12-29T23:59:00Z")
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof CreateAlbumDialog> = (args) => (
+export default {
+    title: 'Albums/CreateAlbumDialog',
+};
+
+type Props = Exclude<CreateDialogSelection, "open">
+
+export const Default: Story<Props> = (props) => (
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='fr'>
-        <CreateAlbumDialog {...args}/>
+        <CreateAlbumDialog onClose={function (): void {
+            throw new Error("Function not implemented.");
+        }} onSubmit={function (): Promise<void> {
+            throw new Error("Function not implemented.");
+        }} onNameChange={function (name: string): void {
+            throw new Error("Function not implemented.");
+        }} onFolderNameChange={function (folderName: string): void {
+            throw new Error("Function not implemented.");
+        }} onWithCustomFolderNameChange={function (withCustom: boolean): void {
+            throw new Error("Function not implemented.");
+        }} onStartsAtStartOfTheDayChange={function (startsAtStart: boolean): void {
+            throw new Error("Function not implemented.");
+        }} onEndsAtEndOfTheDayChange={function (endsAtEnd: boolean): void {
+            throw new Error("Function not implemented.");
+        }} onStartDateChange={function (date: Date | null): void {
+            throw new Error("Function not implemented.");
+        }} onEndDateChange={function (date: Date | null): void {
+            throw new Error("Function not implemented.");
+        }} {...props}/>
     </LocalizationProvider>
 );
 
-// it should display the model with no name, defaulted start and end date (1 week apart), no folder name, "create" button disabled
-export const Empty = Template.bind({});
-Empty.args = {
-    open: true,
-    albumName: "",
-    start: defaultStartDate,
-    end: endDate,
-    customFolderName: "",
-    startsAtStartOfTheDay: true,
-    endsAtEndOfTheDay: true,
-    isCustomFolderNameEnabled: false,
-    isLoading: false,
-    canSubmit: false,
-    onClose: () => {
-    },
-    onSubmit: () => Promise.resolve(),
-    onNameChange: () => {
-    },
-    onStartDateChange: () => {
-    },
-    onEndDateChange: () => {
-    },
-    onFolderNameChange: () => {
-    },
-    onWithCustomFolderNameChange: () => {
-    },
-    onStartsAtStartOfTheDayChange: () => {
-    },
-    onEndsAtEndOfTheDayChange: () => {
-    },
-};
-Empty.parameters = {
-    delay: 300,
+Default.args = {
+    label: 'Hello world',
+    disabled: false,
+    count: 2,
+    colors: ['Red', 'Blue'],
 };
 
-// it should have the "save" button enabled when the name is not empty
-export const WithAName = Template.bind({});
-WithAName.args = {
-    ...Empty.args,
-    albumName: 'Avenger 3',
-    canSubmit: true,
-};
-WithAName.parameters = {
-    delay: 300,
-};
 
-// it should render an error on the Name field when the error albumFolderNameAlreadyTakenErr is raised ; and the error should clear when the name of folder name are updated
-export const NameAlreadyExists = Template.bind({});
-NameAlreadyExists.args = {
-    ...Empty.args,
-    nameError: "Name must be unique",
-};
-NameAlreadyExists.parameters = {
-    delay: 300,
-};
+export const Default2 = () => {
+    const args = {
+        open: true,
+        albumName: "",
+        start: defaultStartDate,
+        end: endDate,
+        customFolderName: "",
+        startsAtStartOfTheDay: true,
+        endsAtEndOfTheDay: true,
+        isCustomFolderNameEnabled: false,
+        isLoading: false,
+        canSubmit: false,
+        onClose: () => {
+        },
+        onSubmit: () => Promise.resolve(),
+        onNameChange: () => {
+        },
+        onStartDateChange: () => {
+        },
+        onEndDateChange: () => {
+        },
+        onFolderNameChange: () => {
+        },
+        onWithCustomFolderNameChange: () => {
+        },
+        onStartsAtStartOfTheDayChange: () => {
+        },
+        onEndsAtEndOfTheDayChange: () => {
+        },
+    };
 
-// it should render an error on the Name field when the error albumFolderNameAlreadyTakenErr is raised ; and the error should clear when the name of folder name are updated
-export const FolderNameAlreadyExists = Template.bind({});
-FolderNameAlreadyExists.args = {
-    ...Empty.args,
-    folderNameError: "Name must be unique",
-    customFolderName: "/avenger-3",
-    isCustomFolderNameEnabled: true,
-};
-FolderNameAlreadyExists.parameters = {
-    delay: 300,
-};
-
-// it should render an error on the Name field when the error albumFolderNameAlreadyTakenErr is raised ; and the error should clear when the name of folder name are updated
-export const StartAndEndDateAreMandatory = Template.bind({});
-StartAndEndDateAreMandatory.args = {
-    ...Empty.args,
-    dateRangeError: "Start date must be before end date",
-};
-StartAndEndDateAreMandatory.parameters = {
-    delay: 300,
-};
-
-// it should render an error on the Name field when the error albumFolderNameAlreadyTakenErr is raised ; and the error should clear when the name of folder name are updated
-export const Loading = Template.bind({});
-Loading.args = {
-    ...WithAName.args,
-    isLoading: true,
-    canSubmit: false,
-};
-Loading.parameters = {
-    storyshots: {disable: true},
-};
-
-// it should render an error on the Name field when the error albumFolderNameAlreadyTakenErr is raised ; and the error should clear when the name of folder name are updated
-export const GenericError = Template.bind({});
-GenericError.args = {
-    ...WithAName.args,
-    error: 'Something weird and different than the known errors.'
-};
-GenericError.parameters = {
-    delay: 300,
-};
-
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='fr'>
+            <CreateAlbumDialog {...args}/>
+        </LocalizationProvider>
+    )
+}
+// InFrame.meta = {iframed: true}
