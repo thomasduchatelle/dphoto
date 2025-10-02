5f17caee50057475d80e6238043d7805a5838dcf
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2025-10-02 11:56:48 +0100

    web - adds ladle and playwright in the WAKU sandbox.
    * adds few stories
    * take and validate snapshots

---
+++ web-waku/playwright.config.ts
@@ -0,0 +1,6 @@
+export default {
+    webServer: {
+        command: "npm run ladle",
+        url: `http://localhost:61000`,
+    },
+};
---
+++ web-waku/src/components/counter.stories.tsx
@@ -0,0 +1,4 @@
+import {Counter} from "./counter";
+
+export const Zero = () => <Counter></Counter>
+export const One = () => <h1>Hi !</h1>
