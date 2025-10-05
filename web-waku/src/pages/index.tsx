dab19bbbbb27a71af3524f71a105949895a68997
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2025-10-05 12:06:40 +0100

    fix waku small app local run

--- web-waku/src/pages/index.tsx
+++ web-waku/src/pages/index.tsx
@@ -1,31 +1,27 @@
-import { Link } from 'waku';
-
-import { Counter } from '../components/counter';
-
 export default async function HomePage() {
-  const data = await getData();
+    const data = await getData();
 
-  return (
-    <div>
-      <title>{data.title}</title>
-      <h1 className="text-4xl font-bold tracking-tight">{data.headline}</h1>
-      <p>{data.body}</p>
-    </div>
-  );
+    return (
+        <div>
+            <title>{data.title}</title>
+            <h1 className="text-4xl font-bold tracking-tight">{data.headline}</h1>
+            <p>{data.body}</p>
+        </div>
+    );
 }
 
 const getData = async () => {
-  const data = {
-    title: 'Waku',
-    headline: 'This is the WAKY page.',
-    body: "Your base path doesn't work.",
-  };
+    const data = {
+        title: 'Waku',
+        headline: 'This is the WAKY page.',
+        body: "Your base path doesn't work.",
+    };
 
-  return data;
+    return data;
 };
 
 export const getConfig = async () => {
-  return {
-    render: 'static',
-  } as const;
+    return {
+        render: 'static',
+    } as const;
 };
