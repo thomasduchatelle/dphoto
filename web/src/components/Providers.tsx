8cd7a48b388f4f11bddfaaa0fa9c0beb17d75925
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2025-10-24 22:37:37 +0100

    it does work now ...

--- web/src/components/Providers.tsx
+++ web/src/components/Providers.tsx
@@ -2,7 +2,6 @@
 
 import {CssBaseline} from "@mui/material";
 import DPhotoTheme from "./DPhotoTheme";
-import {ApplicationContextComponent} from "../core/application";
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import {RouterProvider} from "./ClientRouter";
@@ -10,25 +9,20 @@ import dayjs from "dayjs";
 import fr from "dayjs/locale/fr";
 import {ReactNode} from "react";
 import {ErrorBoundary} from "./ErrorBoundary";
-import {Provider} from 'jotai'
 
 dayjs.locale(fr)
 
 export const Providers = ({children}: { children: ReactNode }) => {
     return (
         <ErrorBoundary>
-            <Provider>
-                <DPhotoTheme>
-                    <CssBaseline/>
-                    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='fr'>
-                        <ApplicationContextComponent>
-                            <RouterProvider>
-                                {children}
-                            </RouterProvider>
-                        </ApplicationContextComponent>
-                    </LocalizationProvider>
-                </DPhotoTheme>
-            </Provider>
+            <DPhotoTheme>
+                <CssBaseline/>
+                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='fr'>
+                    <RouterProvider>
+                        {children}
+                    </RouterProvider>
+                </LocalizationProvider>
+            </DPhotoTheme>
         </ErrorBoundary>
     );
 };
