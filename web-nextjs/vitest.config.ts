92344c4ee45faca0b684c838d4e8d41d92af6796
Thomas Duchatelle <thomas.duchatelle@underwriteme.co.uk>
2026-02-13 09:52:19 +0000

    fix tests +next

--- web-nextjs/vitest.config.ts
+++ web-nextjs/vitest.config.ts
@@ -12,27 +12,27 @@ export default defineConfig({
         environment: 'jsdom',
         globals: true,
         setupFiles: [],
-        projects: [{
-            extends: true,
-            plugins: [
-                // The plugin will run tests for the stories defined in your Storybook config
-                // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-                storybookTest({
-                    configDir: path.join(dirname, '.storybook')
-                })],
-            test: {
-                name: 'storybook',
-                browser: {
-                    enabled: true,
-                    headless: true,
-                    provider: playwright({}),
-                    instances: [{
-                        browser: 'chromium'
-                    }]
-                },
-                setupFiles: ['.storybook/vitest.setup.ts']
-            }
-        }]
+        // projects: [{
+        //     extends: true,
+        //     plugins: [
+        //         // The plugin will run tests for the stories defined in your Storybook config
+        //         // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+        //         storybookTest({
+        //             configDir: path.join(dirname, '.storybook')
+        //         })],
+        //     test: {
+        //         name: 'storybook',
+        //         browser: {
+        //             enabled: true,
+        //             headless: true,
+        //             provider: playwright({}),
+        //             instances: [{
+        //                 browser: 'chromium'
+        //             }]
+        //         },
+        //         setupFiles: ['.storybook/vitest.setup.ts']
+        //     }
+        // }]
     },
     resolve: {
         alias: {
